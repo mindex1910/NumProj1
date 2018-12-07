463f0e018a0a9fe6487f3712bb5dd48e8cf42e98
Schoefl <amanda.schoefl@student.tuwien.ac.at>
2018-12-07 17:02:22 +0100

    duffyInt implementiert

--- aufgabe_3_12.m
+++ aufgabe_3_12.m
@@ -1,6 +1,7 @@
 
 f=@(x,y) 1;
-%f=@(x,y) x*y;
-quad_int = quadInt(f,3);
-trig_int = trigInt(f,3);
+%f=@(x,y) (y);
+quad_int = quadInt(f,3)
+trig_int = trigInt(f,3)
+duffy_int = duffyInt(f,3,1,0,1) %a1=1, b1=0, b2=1
 
